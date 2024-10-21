44b785399861017be83a6b1ced6c0e9a012c285d
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2024-10-21 09:36:54 +0200

    test

---
+++ src/TVSeries.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class TVSeries {
+    private String name;
+    private int rating;
+    private ArrayList<Integer> episodes = new ArrayList<>();
+
+    public TVSeries(String inName) {
+
+    }
+
+    public void changeRating(int newRating){
+
+    }
+    public void addEpisode(int episode){}
+
+
+}
